e84c824ecdf68927e5115b9f511503f0f2d8fb5b
Klima77 <kacper@frozenway.games>
2024-03-05 01:39:58 +0100

    fix all shit

--- Efektywność algorytmów - eksperyment/AlgorithmsBenchmarks.cs
+++ Efektywność algorytmów - eksperyment/AlgorithmsBenchmarks.cs
@@ -1,5 +1,4 @@
 ﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Configs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,56 +7,41 @@ using System.Threading.Tasks;
 
 namespace Efektywność_algorytmów___eksperyment
 {
-    [MemoryDiagnoser]
     public class AlgorithmsBenchmarks
     {
-        private int[] randomArray;
-        private int[] sortedArray;
-        private int[] reversedArray;
-        private int[] almostSortedArray;
-        private int[] fewUniqueArray;
-
-        [Params(1000)]
-        public int ArraySize;
-
-        [GlobalSetup]
-        public void Setup()
+        [Benchmark]
+        public void InsertionSortRandom()
         {
-            randomArray = Generators.GenerateRandom(ArraySize, 0, 1000);
-            sortedArray = Generators.GenerateSorted(ArraySize, 0, 1000);
-            reversedArray = Generators.GenerateReversed(ArraySize, 0, 1000);
-            almostSortedArray = Generators.GenerateAlmostSorted(ArraySize, 0, 1000, 10);
-            fewUniqueArray = Generators.GenerateFewUnique(ArraySize, 0, 1000, 5);
+            int[] array = Generators.GenerateRandom(1000, 1, 1000);
+            SortingAlgorithms.InsertionSort(array);
         }
 
         [Benchmark]
-        public void InsertionSortRandom() => SortingAlgorithms.InsertionSort(randomArray.Clone() as int[]);
+        public void InsertionSortSorted()
+        {
+            int[] array = Generators.GenerateSorted(1000, 1, 1000);
+            SortingAlgorithms.InsertionSort(array);
+        }
 
         [Benchmark]
-        public void InsertionSortSorted() => SortingAlgorithms.InsertionSort(sortedArray.Clone() as int[]);
+        public void InsertionSortReversed()
+        {
+            int[] array = Generators.GenerateReversed(1000, 1, 1000);
+            SortingAlgorithms.InsertionSort(array);
+        }
 
         [Benchmark]
-        public void InsertionSortReversed() => SortingAlgorithms.InsertionSort(reversedArray.Clone() as int[]);
+        public void InsertionSortAlmostSorted()
+        {
+            int[] array = Generators.GenerateAlmostSorted(1000, 1, 1000, 20);
+            SortingAlgorithms.InsertionSort(array);
+        }
 
         [Benchmark]
-        public void InsertionSortAlmostSorted() => SortingAlgorithms.InsertionSort(almostSortedArray.Clone() as int[]);
-
-        [Benchmark]
-        public void InsertionSortFewUnique() => SortingAlgorithms.InsertionSort(fewUniqueArray.Clone() as int[]);
-
-        [Benchmark]
-        public void QuickSortRandom() => SortingAlgorithms.QuickSort(randomArray.Clone() as int[]);
-
-        [Benchmark]
-        public void QuickSortSorted() => SortingAlgorithms.QuickSort(sortedArray.Clone() as int[]);
-
-        [Benchmark]
-        public void QuickSortReversed() => SortingAlgorithms.QuickSort(reversedArray.Clone() as int[]);
-
-        [Benchmark]
-        public void QuickSortAlmostSorted() => SortingAlgorithms.QuickSort(almostSortedArray.Clone() as int[]);
-
-        [Benchmark]
-        public void QuickSortFewUnique() => SortingAlgorithms.QuickSort(fewUniqueArray.Clone() as int[]);
+        public void InsertionSortFewUnique()
+        {
+            int[] array = Generators.GenerateFewUnique(1000, 1, 1000, 3);
+            SortingAlgorithms.InsertionSort(array);
+        }
     }
 }
